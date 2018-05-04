4fc69108e5eecc680bcd176c742c8654418989f6
Tim Roush <perty_slick@hotmail.com>
2018-05-04 15:23:13 -0700

    Part 1 - Completion of foundation for implementation of version one.

--- KWICv1/src/main/java/kwicIndex.java
+++ KWICv1/src/main/java/kwicIndex.java
@@ -1,7 +1,41 @@
+import java.util.ArrayList;
+
 public class kwicIndex {
 
-
     public static void main(String[] args) {
+        // Get input from file
+        // Perform circular shift operation on resulting input
+        // Sort those results
+        // Output results
+    }
 
+    private static ArrayList<String> readLines() {
+        // Open input file
+        // Read all lines of text into an ArrayList<String>
+        // Return the resulting ArrayList
+        return null;
+    }
+
+    private static void circularShift(ArrayList<String> lines) {
+        // For every lines in supplied collection:
+        // Perform a circular shift operation
+    }
+
+    private static String circularShiftLine(String line) {
+        // Split line into an array
+        // Store the first word
+        // Build a new string with the remaining words (all except first)
+        // Append first word to the end of new string
+        // Return string
+        return null;
+    }
+
+    private static void alphabetize(ArrayList<String> lines) {
+        // Sort the Collection of lines
+    }
+
+    private static void writeLines(ArrayList<String> lines) {
+        // Write lines as output to console
+        // Write lines as output to file
     }
 }
